13916d1c7cdb3a9fee29711e2896d605f2ccd09d
SatoruKawashima <whiterumia@gmail.com>
2026-07-27 22:05:06 +0900

    first commit

---
+++ 01_number_game/main.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() {
+    int32_t number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("You entered: %d\n", number);
+}
