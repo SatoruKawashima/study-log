1ce4c0a80636333d9bf14e29e012e63c07d9d9e2
SatoruKawashima <whiterumia@gmail.com>
2026-07-28 23:16:04 +0900

    feat:数字あてゲームを試作

--- 01_number_game/main.c
+++ 01_number_game/main.c
@@ -1,10 +1,33 @@
+#include <time.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h> 
 
 int main() {
-    int32_t number;
-    printf("Enter a number: ");
-    scanf("%d", &number);
-    printf("You entered: %d\n", number);
+
+    srand(time(NULL));
+
+    int number = 0;
+    int answer = rand() %1000;
+    int min = 0;
+    int max = 1000;
+    int status;
+    
+    do {
+        printf("%d から %d の間で整数を入力してください: ", min, max);
+        status = scanf("%d", &number);
+        if(status != 1) {printf("\nエラー:整数ではありません\n") ; while(getchar() != '\n'); continue; }
+        if(number < min || number > max) {printf("エラー:範囲外の数値です\n"); while(getchar() != '\n'); continue; }
+        if(number < answer) {
+            printf("もっと大きい数字です！ \a\n");
+        } else if (number > answer) {
+            printf("もっと小さい数字です！ \a\n");
+        } 
+    } while (answer != number);
+
+
+    printf("正解です！ \n");
+    
+
+    return 0;
 }
