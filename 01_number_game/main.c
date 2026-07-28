018c89948de3574bde4668f09a62ee62ad7915e0
SatoruKawashima <whiterumia@gmail.com>
2026-07-28 23:31:09 +0900

    fix:エラーについて空文字対策を追加

--- 01_number_game/main.c
+++ 01_number_game/main.c
@@ -12,12 +12,13 @@ int main() {
     int min = 0;
     int max = 1000;
     int status;
-    
+    int c;
+
     do {
         printf("%d から %d の間で整数を入力してください: ", min, max);
         status = scanf("%d", &number);
-        if(status != 1) {printf("\nエラー:整数ではありません\n") ; while(getchar() != '\n'); continue; }
-        if(number < min || number > max) {printf("エラー:範囲外の数値です\n"); while(getchar() != '\n'); continue; }
+        if(status != 1) {printf("\nエラー:整数ではありません\n") ; while ((c = getchar()) != '\n' && c != EOF); continue; }
+        if(number < min || number > max) {printf("エラー:範囲外の数値です\n"); while ((c = getchar()) != '\n' && c != EOF); continue; }
         if(number < answer) {
             printf("もっと大きい数字です！ \a\n");
         } else if (number > answer) {
